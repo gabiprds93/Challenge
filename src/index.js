daaef2a161d05fcc47722d1e6e4901f52e9bf01d
Gabriela <gabiprds_93@hotmail.com>
2020-10-08 18:54:20 -0500

    initial script

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class FetchGithub extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      location: ''
+    };
+  
+  componentDidMount() {
+    fetch('https://api.github.com/users/workshopsjsmvd')
+      .then(res => {
+        this.setState({
+          name: res.name,
+          location: res.location
+        })
+      });
+  }
+
+  render() {
+    return [
+      <h1 key="name">{`Nombre: ${this.state.name}`}</h1>,
+      <h2 key="location">{`País: ${this.state.local}`}</h2>
+    ];
+  }
+}
+
+ReactDOM.render(
+  <FetchGithub />,
+  document.getElementById('root')
+);
