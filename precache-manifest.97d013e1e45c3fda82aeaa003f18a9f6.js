ca354c0b66e4285eb695bbfd98fcff575fbfc5e9
Gabriela <gabiprds_93@hotmail.com>
2020-10-08 20:47:56 -0500

    Updates

---
+++ precache-manifest.97d013e1e45c3fda82aeaa003f18a9f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133b7485e5e08216fd92b79d2a9d23fc",
+    "url": "/Challenge/index.html"
+  },
+  {
+    "revision": "aa3d9195390a91d8b4cd",
+    "url": "/Challenge/static/js/2.f481aaac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Challenge/static/js/2.f481aaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "070ae9723d74de7f7c0d",
+    "url": "/Challenge/static/js/main.43f17042.chunk.js"
+  },
+  {
+    "revision": "4e267d83af88839bd800",
+    "url": "/Challenge/static/js/runtime-main.c02a924d.js"
+  }
+]);
